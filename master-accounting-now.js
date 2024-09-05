40002eb8b9326afa89d959ad18abefda709b9b71
thefunneldiva <thefunneldiva@gmail.com>
2024-09-05 16:47:01 +0800

    Update master-accounting-now.js

--- master-accounting-now.js
+++ master-accounting-now.js
@@ -1,23 +1,23 @@
-// Select the checkbox using the name attribute
-var checkbox = document.querySelector('input[name="terms_and_conditions"]');
-setInterval(function () {
+// Set the interval
+var interval = setInterval(function () {
+  var checkbox = document.querySelector('input[name="terms_and_conditions"]');
   if (checkbox) {
-        checkbox.click();
-        checkbox.checked = true;  // Set it to checked by default
-    console.log('Must checked')
+    if (!checkbox.checked) {
+      checkbox.click();  // Simulate the click
+      checkbox.checked = true;  // Make sure it stays checked
+    } else {
+      // Stop the interval if the checkbox is already checked
+      clearInterval(interval);
+      console.log('Checkbox is checked, interval stopped');
+    }
   }
-}, 1000)
+}, 1000);
 
-// Target the checkbox using its name attribute
-// var checkbox = document.querySelector('input[name="terms_and_conditions"]');
-
-// // After 17 seconds (17000 milliseconds), click and check the checkbox
-// setTimeout(function() {
-//   if (checkbox) {
-//     checkbox.click();  // This will simulate a user click
-//     checkbox.checked = true;  // Make sure it stays checked
-//   }
-//   console.log('Must checked');
-// }, 17000);
-
-console.log("JS From Github");
+// Add an event listener to stop the interval when the checkbox is manually clicked
+var checkbox = document.querySelector('input[name="terms_and_conditions"]');
+checkbox.addEventListener('click', function() {
+  if (checkbox.checked) {
+    clearInterval(interval);  // Stop the interval if it's checked
+    console.log('Checkbox manually clicked, interval stopped');
+  }
+});
