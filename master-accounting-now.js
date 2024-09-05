54a4059ab8149e240e818f0553f5da4c2f508445
thefunneldiva <thefunneldiva@gmail.com>
2024-09-05 16:12:25 +0800

    initial commit

---
+++ master-accounting-now.js
@@ -0,0 +1,5 @@
+// Select the checkbox using the name attribute
+var checkbox = document.querySelector('input[name="terms_and_conditions"]');
+if (checkbox) {
+  checkbox.checked = true;  // Set it to checked by default
+}
