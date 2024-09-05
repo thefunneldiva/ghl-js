f4e617c3abfb1de709e3b88c6f407e075884d817
thefunneldiva <thefunneldiva@gmail.com>
2024-09-05 16:22:05 +0800

    Update master-accounting-now.js
    asds

--- master-accounting-now.js
+++ master-accounting-now.js
@@ -1,7 +1,9 @@
 // Select the checkbox using the name attribute
-var checkbox = document.querySelector('input[name="terms_and_conditions"]');
-if (checkbox) {
-  checkbox.checked = true;  // Set it to checked by default
-}
+setInterval(function () {
+  var checkbox = document.querySelector('input[name="terms_and_conditions"]');
+  if (checkbox) {
+    checkbox.checked = true;  // Set it to checked by default
+  }
+}, 1000)
 
 console.log("JS From Github");
