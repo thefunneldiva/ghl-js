5c32d3b942a8a964089cb2586943cf7150e66c22
thefunneldiva <thefunneldiva@gmail.com>
2024-09-05 16:40:04 +0800

    Update master-accounting-now.js

--- master-accounting-now.js
+++ master-accounting-now.js
@@ -1,21 +1,23 @@
 // Select the checkbox using the name attribute
-// setInterval(function () {
-//   var checkbox = document.querySelector('input[name="terms_and_conditions"]');
-//   if (checkbox) {
-//     checkbox.checked = true;  // Set it to checked by default
-//   }
-// }, 1000)
+var checkbox = document.querySelector('input[name="terms_and_conditions"]');
+setInterval(function () {
+  if (checkbox) {
+        checkbox.click();
+        checkbox.checked = true;  // Set it to checked by default
+    console.log('Must checked')
+  }
+}, 1000)
 
 // Target the checkbox using its name attribute
-var checkbox = document.querySelector('input[name="terms_and_conditions"]');
+// var checkbox = document.querySelector('input[name="terms_and_conditions"]');
 
-// After 17 seconds (17000 milliseconds), click and check the checkbox
-setTimeout(function() {
-  if (checkbox) {
-    checkbox.click();  // This will simulate a user click
-    checkbox.checked = true;  // Make sure it stays checked
-  }
-  console.log('Must checked');
-}, 17000);
+// // After 17 seconds (17000 milliseconds), click and check the checkbox
+// setTimeout(function() {
+//   if (checkbox) {
+//     checkbox.click();  // This will simulate a user click
+//     checkbox.checked = true;  // Make sure it stays checked
+//   }
+//   console.log('Must checked');
+// }, 17000);
 
 console.log("JS From Github");
