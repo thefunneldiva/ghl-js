737a8299fc93fcf70e2b872a1b33c936b2510cdb
thefunneldiva <thefunneldiva@gmail.com>
2024-10-10 11:21:49 +0800

    Update satva-clone.js

--- satva-clone.js
+++ satva-clone.js
@@ -1,10 +1,8 @@
-<script>
-	$(document).ready(function () {
-      	AOS.init();
-      	let image1 = $('#image-h2G9Lk83H- picture img').attr('src');
-      	$('#image-h2G9Lk83H- picture img').remove()
-    	$('#image-h2G9Lk83H- picture').append(
-        	`<img src="${image1}" alt="" style="" class="img-none img-border-none img-effects-none hl-optimized mw-100" loading="lazy" data-aos-duration="1000" data-aos="fade-right">`
-        )
-    })
-</script>
+$(document).ready(function () {
+    AOS.init();
+    let image1 = $('#image-h2G9Lk83H- picture img').attr('src');
+    $('#image-h2G9Lk83H- picture img').remove()
+    $('#image-h2G9Lk83H- picture').append(
+        `<img src="${image1}" alt="" style="" class="img-none img-border-none img-effects-none hl-optimized mw-100" loading="lazy" data-aos-duration="1000" data-aos="fade-right">`
+    )
+})
