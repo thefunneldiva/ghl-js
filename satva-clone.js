18488204a4e63238d6ac66634543aedc3e676255
thefunneldiva <thefunneldiva@gmail.com>
2024-10-10 15:56:22 +0800

    Update satva-clone.js

--- satva-clone.js
+++ satva-clone.js
@@ -21,19 +21,31 @@ $(document).ready(function () {
     //     `<img src="${image2}" alt="" style="" class="img-none img-border-none img-effects-none hl-optimized mw-100" loading="lazy" data-aos-duration="1000" data-aos="fade-right">`
     // )
 
-    
-    $('#image-i9KMI2-mlE').attr('data-aos', 'fade-left');
+    // let image3 = $('#image-jgdn2qCEyQ picture img').attr('src');
+    // $('#image-jgdn2qCEyQ picture img').remove()
+    // $('#image-jgdn2qCEyQ picture').append(
+    //     `<img src="${image3}" alt="" style="" class="img-none img-border-none img-effects-none hl-optimized mw-100" loading="lazy" data-aos-duration="1000" data-aos="fade-right">`
+    // )
 
-    let image3 = $('#image-jgdn2qCEyQ picture img').attr('src');
-    $('#image-jgdn2qCEyQ picture img').remove()
-    $('#image-jgdn2qCEyQ picture').append(
-        `<img src="${image3}" alt="" style="" class="img-none img-border-none img-effects-none hl-optimized mw-100" loading="lazy" data-aos-duration="1000" data-aos="fade-right">`
-    )
+    $('#image-i9KMI2-mlE').attr('data-aos', 'fade-right');
+    $('#image-jgdn2qCEyQ').attr('data-aos', 'fade-right');
 
     $('#row-SOIuV8U199').attr('data-aos', 'fade-left');
     $('#row-dzUxyhNtqU').attr('data-aos', 'fade-left');
     $('#row-JnwUBtFuRF').attr('data-aos', 'fade-left');
     $('#row-t8HAeEoOCa').attr('data-aos', 'fade-left');
-    $('#image-9XXb78QjeW').attr('data-aos', 'fade-left');
+    $('#image-Ne-d08IEk-').attr('data-aos', 'fade-up');
+    $('#paragraph-D_tpy2besL').attr('data-aos', 'fade-right');
+    $('#image-GRhdu8s0d6').attr('data-aos', 'fade-left');
+    $('#image-up_ukj8X_m').attr('data-aos', 'fade-right');
+    $('#image-xXA5pm0K_A').attr('data-aos', 'fade-left');
+    $('#image-ROoJqV57xo').attr('data-aos', 'fade-right');
+    $('#image-nokW_Iue52').attr('data-aos', 'fade-down');
+    $('#image-nokW_Iue52').attr('data-aos', 'fade-down');
+    $('#image-6bW8nUUs3h').attr('data-aos', 'fade-left');
+    $('#image-LCYsYQxo1X').attr('data-aos', 'fade-right');
+    $('#col-zukzgU-mPA').attr('data-aos', 'fade-right');
+    $('#image-AnSj6IqcKH').attr('data-aos', 'fade-left');
+    $('#image-_CBHevbq5A').attr('data-aos', 'fade-left');
     
 })
